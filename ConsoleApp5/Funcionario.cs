3566a9c79e8a7407f74f9b47819d590c9206fb5a
Natalia Ramos <nathy.ramos564@gmail.com>
2022-01-24 12:26:26 -0300

    nao sei

---
+++ ConsoleApp5/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp5
+{
+     class Funcionario
+    {
+        public string Nome { get; set; }
+        public double SalarioBruto { get; set; }
+        public double Imposto { get; set; }
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * porcentagem;
+        }
+        
+    }
+       
+
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            func.Nome = "Joao Silva";
+            func.SalarioBruto = 6000;
+            func.Imposto = 1000;
+
+            Console.WriteLine($"Funcionário: {func.Nome}, R${func.SalarioLiquido():N2}");
+            Console.WriteLine("Digite  a porcentagem para aumentar o salário:");
+            double porcentagem = double.Parse(Console.ReadLine()) / 100;
+            func.AumentarSalario(porcentagem);
+            Console.WriteLine($"Dados atualizados: {func.Nome}, R${func.SalarioLiquido():N2}");
+
+        }
+    }
+}
